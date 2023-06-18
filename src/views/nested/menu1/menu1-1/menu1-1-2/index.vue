ea467b9e54b21215d3e336d6504bcc27a347230b
hastune39  //你的用户名 <1718914845@qq.com  //git>
2023-06-18 10:50:19 +0800

    fix: test

--- src/views/nested/menu1/menu1-1/menu1-1-2/index.vue
+++ src/views/nested/menu1/menu1-1/menu1-1-2/index.vue
@@ -1,51 +1,135 @@
 <script setup lang="ts">
 import { ref } from "vue";
-// import { useI18n } from "vue-i18n";
-// import { useRoute } from "vue-router";
-import { PureTable } from "@pureadmin/table";
-import { useColumns } from "./columns";
+import { useDept } from "./hook";
+import { PureTableBar } from "@/components/RePureTableBar";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+
+// import Delete from "@iconify-icons/ep/delete";
+import EditPen from "@iconify-icons/ep/edit-pen";
+import Search from "@iconify-icons/ep/search";
+import Refresh from "@iconify-icons/ep/refresh";
+// import AddFill from "@iconify-icons/ri/add-circle-line";
+
 defineOptions({
   name: "Menu1-1-2"
 });
+
+const formRef = ref();
 const tableRef = ref();
 const {
+  form,
   loading,
   columns,
   dataList,
-  pagination,
-  loadingConfig,
-  adaptiveConfig,
-  onSizeChange,
-  onCurrentChange
-} = useColumns();
+  onSearch,
+  resetForm,
+  openDialog,
+  // handleDelete,
+  handleSelectionChange
+} = useDept();
 </script>
 
 <template>
-  <div>
-    <el-card shadow="never">
-      <div class="dark:text-white">
+  <div class="main">
+    <el-form
+      ref="formRef"
+      :inline="true"
+      :model="form"
+      class="search-form bg-bg_color w-[99/100] pl-8 pt-[12px]"
+    >
+      <el-form-item label="部门名称：" prop="name">
+        <el-input
+          v-model="form.name"
+          placeholder="请输入部门名称"
+          clearable
+          class="!w-[200px]"
+        />
+      </el-form-item>
+      <el-form-item label="状态：" prop="status">
+        <el-select
+          v-model="form.status"
+          placeholder="请选择状态"
+          clearable
+          class="!w-[180px]"
+        >
+          <el-option label="启用" :value="1" />
+          <el-option label="停用" :value="0" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          :icon="useRenderIcon(Search)"
+          :loading="loading"
+          @click="onSearch"
+        >
+          搜索
+        </el-button>
+        <el-button :icon="useRenderIcon(Refresh)" @click="resetForm(formRef)">
+          重置
+        </el-button>
+      </el-form-item>
+    </el-form>
+
+    <PureTableBar
+      title="码垛设备"
+      :columns="columns"
+      :tableRef="tableRef?.getTableRef()"
+      @refresh="onSearch"
+    >
+      <!-- <template #buttons>
+        <el-button
+          type="primary"
+          :icon="useRenderIcon(AddFill)"
+          @click="openDialog()"
+        >
+          新增部门
+        </el-button>
+      </template> -->
+      <template v-slot="{ size, dynamicColumns }">
         <pure-table
+          style="width: 9%; margin-left: 2%"
           ref="tableRef"
           border
           adaptive
-          :adaptiveConfig="adaptiveConfig"
+          :adaptiveConfig="{ offsetBottom: 32 }"
+          align-whole="center"
           row-key="id"
-          alignWhole="center"
           showOverflowTooltip
+          table-layout="auto"
+          default-expand-all
           :loading="loading"
-          :loading-config="loadingConfig"
-          :data="
-            dataList.slice(
-              (pagination.currentPage - 1) * pagination.pageSize,
-              pagination.currentPage * pagination.pageSize
-            )
-          "
-          :columns="columns"
-          :pagination="pagination"
-          @page-size-change="onSizeChange"
-          @page-current-change="onCurrentChange"
-        />
-      </div>
-    </el-card>
+          :size="size"
+          :data="dataList"
+          :columns="dynamicColumns"
+          :header-cell-style="{
+            background: 'var(--el-table-row-hover-bg-color)',
+            color: 'var(--el-text-color-primary)'
+          }"
+          @selection-change="handleSelectionChange"
+        >
+          <template #operation="{ row }">
+            <el-button
+              class="reset-margin"
+              link
+              type="primary"
+              :size="size"
+              :icon="useRenderIcon(EditPen)"
+              @click="openDialog('更改设置', row)"
+            >
+              更改设置
+            </el-button>
+          </template>
+        </pure-table>
+      </template>
+    </PureTableBar>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.search-form {
+  :deep(.el-form-item) {
+    margin-bottom: 12px;
+  }
+}
+</style>
